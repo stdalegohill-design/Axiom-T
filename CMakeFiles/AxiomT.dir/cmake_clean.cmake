cae707329d88fdb0a6cae00d5a61f30829c3d904
stdalegohill-desing <std.alegohill@gmail.com>
2026-03-15 21:59:23 -0500

    Phase 1 complete - UnitSystem, NumericalSolver - IdealGas

---
+++ CMakeFiles/AxiomT.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "AxiomT.exe"
+  "AxiomT.exe.manifest"
+  "AxiomT.pdb"
+  "CMakeFiles/AxiomT.dir/main.cpp.obj"
+  "CMakeFiles/AxiomT.dir/main.cpp.obj.d"
+  "libAxiomT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AxiomT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/ThermoTests.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ThermoTests.dir/tests/catch2/catch_amalgamated.cpp.obj"
+  "CMakeFiles/ThermoTests.dir/tests/catch2/catch_amalgamated.cpp.obj.d"
+  "CMakeFiles/ThermoTests.dir/tests/test_ideal_gas.cpp.obj"
+  "CMakeFiles/ThermoTests.dir/tests/test_ideal_gas.cpp.obj.d"
+  "CMakeFiles/ThermoTests.dir/tests/test_numerical_solver.cpp.obj"
+  "CMakeFiles/ThermoTests.dir/tests/test_numerical_solver.cpp.obj.d"
+  "CMakeFiles/ThermoTests.dir/tests/test_pressure.cpp.obj"
+  "CMakeFiles/ThermoTests.dir/tests/test_pressure.cpp.obj.d"
+  "CMakeFiles/ThermoTests.dir/tests/test_temperature.cpp.obj"
+  "CMakeFiles/ThermoTests.dir/tests/test_temperature.cpp.obj.d"
+  "CMakeFiles/ThermoTests.dir/tests/test_units.cpp.obj"
+  "CMakeFiles/ThermoTests.dir/tests/test_units.cpp.obj.d"
+  "ThermoTests.exe"
+  "ThermoTests.exe.manifest"
+  "ThermoTests.pdb"
+  "libThermoTests.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ThermoTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
